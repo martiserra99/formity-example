677d6eaf32333ebeeb3e57b93d92289bdd8b112e
Marti Serra <martiserramolina99@gmail.com>
2023-12-05 14:18:11 +0100

    Update text-field

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { FormProvider, useForm } from "react-hook-form";
+
 import { Box, Flex } from "@radix-ui/themes";
 
 import Center from "./components/center";
@@ -5,70 +7,34 @@ import Card from "./components/card";
 import Heading from "./components/typography/heading";
 import Text from "./components/typography/text";
 import TextField from "./components/form/text-field";
-import TextArea from "./components/form/text-area";
-import Select from "./components/form/select";
-import RadioGroup from "./components/form/radio-group";
-import CheckboxGroup from "./components/form/checkbox-group";
 import Button from "./components/button";
 
 function App() {
+  const form = useForm({
+    defaultValues: {
+      name: "John Doe",
+    },
+  });
+
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <Center>
       <Card>
-        <Box mb="4">
-          <Heading>Hello, world!</Heading>
-          <Text>This is a card.</Text>
-        </Box>
-        <Flex direction="column" gap="4">
-          <TextField label="Name" defaultValue="Marti" />
-          <TextArea label="Message" defaultValue="Hello, world!" />
-          <Select
-            label="Fruit"
-            type="items"
-            list={[
-              { value: "apple", label: "Apple" },
-              { value: "orange", label: "Orange", disabled: true },
-            ]}
-            defaultValue="apple"
-          />
-          <Select
-            label="Fruit"
-            type="groups"
-            list={[
-              {
-                label: "Fruits",
-                items: [
-                  { value: "apple", label: "Apple", disabled: true },
-                  { value: "orange", label: "Orange" },
-                ],
-              },
-              {
-                label: "Vegetables",
-                items: [
-                  { value: "carrot", label: "Carrot", disabled: true },
-                  { value: "celery", label: "Celery" },
-                ],
-              },
-            ]}
-            defaultValue="orange"
-          />
-          <RadioGroup
-            label="Toggle"
-            list={[
-              { value: "man", label: "Man" },
-              { value: "woman", label: "Woman" },
-            ]}
-            defaultValue="man"
-          />
-          <CheckboxGroup
-            label="Fruits"
-            list={[
-              { value: "apple", label: "Apple" },
-              { value: "orange", label: "Orange" },
-            ]}
-          />
-          <Button>Click me</Button>
-        </Flex>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <Box mb="4">
+              <Heading>Hello, world!</Heading>
+              <Text>This is a card.</Text>
+            </Box>
+            <Flex direction="column" gap="4">
+              <TextField label="Name" name="name" />
+              <Button>Click me</Button>
+            </Flex>
+          </form>
+        </FormProvider>
       </Card>
     </Center>
   );
--- src/components/form/text-field.jsx
+++ src/components/form/text-field.jsx
@@ -1,18 +1,17 @@
 import styles from "./text-field.module.css";
 
+import { useFormContext } from "react-hook-form";
+
 import { Text, TextField as RadixTextField } from "@radix-ui/themes";
 
-export default function TextField({ label, defaultValue }) {
+export default function TextField({ label, name }) {
+  const { register } = useFormContext();
   return (
     <Text as="label" className={styles.label}>
       <Text as="div" size="2" mb="1" weight="bold">
         {label}
       </Text>
-      <RadixTextField.Input
-        size="2"
-        variant="surface"
-        defaultValue={defaultValue}
-      />
+      <RadixTextField.Input size="2" variant="surface" {...register(name)} />
     </Text>
   );
 }
