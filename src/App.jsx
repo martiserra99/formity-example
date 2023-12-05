9902b048a09f4220a5dd368e3a76577e4b4cb436
Marti Serra <martiserramolina99@gmail.com>
2023-12-05 19:25:53 +0100

    Added formity

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,64 @@
-import { FormProvider, useForm } from "react-hook-form";
-
-import { Flex, Grid } from "@radix-ui/themes";
+import { Formity } from "formity";
 
 import Center from "./components/center";
 import Card from "./components/card";
-import Heading from "./components/typography/heading";
-import Text from "./components/typography/text";
-import TextField from "./components/form/text-field";
-import TextArea from "./components/form/text-area";
-import Select from "./components/form/select";
-import RadioGroup from "./components/form/radio-group";
-import CheckboxGroup from "./components/form/checkbox-group";
-import Slider from "./components/form/slider";
-import Range from "./components/form/range";
-import Button from "./components/button";
-import LayoutForm from "./components/layout-form";
+
+const form = [
+  {
+    form: {
+      defaultValues: {
+        name: "",
+        surname: "",
+      },
+      resolver: {
+        name: [[{ _$ne: ["_$name", ""] }, "Required"]],
+        surname: [[{ _$ne: ["_$surname", ""] }, "Required"]],
+      },
+      render: [
+        {
+          LayoutForm: {
+            heading: "Formity",
+            text: "A React form library",
+            fields: [
+              {
+                TextField: {
+                  name: "name",
+                  label: "Name",
+                  placeholder: "Enter your name",
+                },
+              },
+              {
+                TextField: {
+                  name: "surname",
+                  label: "Surname",
+                  placeholder: "Enter your surname",
+                },
+              },
+            ],
+            buttons: [
+              {
+                Button: {
+                  type: "submit",
+                  children: "Submit",
+                },
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+  { return: { name: { $concat: ["$name", " ", "$surname"] } } },
+];
 
 function App() {
-  const form = useForm({
-    defaultValues: {
-      name: "John Doe",
-      message: "Hello, world!",
-      sport: "option-1",
-      gender: "man",
-      hobbies: ["reading", "coding"],
-      rating: 30,
-      ages: [30, 40],
-    },
-  });
-
   function handleSubmit(data) {
     console.log(data);
   }
-
   return (
     <Center>
       <Card>
-        <FormProvider {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)}>
-            <LayoutForm
-              heading="Hello"
-              text="Goodbye"
-              fields={[<TextField key={0} name="name" label="Name" />]}
-              buttons={[<Button key={0}>Next</Button>]}
-            />
-          </form>
-        </FormProvider>
+        <Formity json={form} onSubmit={handleSubmit} />
       </Card>
     </Center>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,36 @@ import { Theme } from "@radix-ui/themes";
 
 import { FormityProvider } from "formity";
 
+import LayoutForm from "./components/layout-form";
+import Heading from "./components/typography/heading";
+import Text from "./components/typography/text";
+import TextField from "./components/form/text-field";
+import TextArea from "./components/form/text-area";
+import Select from "./components/form/select";
+import RadioGroup from "./components/form/radio-group";
+import CheckboxGroup from "./components/form/checkbox-group";
+import Slider from "./components/form/slider";
+import Range from "./components/form/range";
+import Button from "./components/button";
+
+const components = {
+  LayoutForm,
+  Heading,
+  Text,
+  TextField,
+  TextArea,
+  Select,
+  RadioGroup,
+  CheckboxGroup,
+  Slider,
+  Range,
+  Button,
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Theme>
-      <FormityProvider>
+      <FormityProvider components={components}>
         <App />
       </FormityProvider>
     </Theme>
