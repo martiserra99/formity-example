1d02e62a42b6a91f6b61704b4c8b96a034e7958e
Marti Serra <martiserramolina99@gmail.com>
2023-12-26 17:11:01 +0100

    Added padding

--- src/components/center.jsx
+++ src/components/center.jsx
@@ -3,5 +3,9 @@ import styles from "./center.module.css";
 import { Box } from "@radix-ui/themes";
 
 export default function Center({ children }) {
-  return <Box className={styles.center}>{children}</Box>;
+  return (
+    <Box className={styles.center} p="4">
+      {children}
+    </Box>
+  );
 }
